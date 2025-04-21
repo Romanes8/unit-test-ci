91673fa075ddddfef05d728e1236aa9dc5c0ca37
Roman Saklakov <Roman-S.89@mail.ru>
2025-04-21 13:49:31 +0300

    update 2 webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-
-const path = require('node:path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
+    filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
+    library: 'ajs', // название нашей библиотеки
+    libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
+    libraryExport: 'default', // экспортируется имя default
+    globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
   },
   module: {
     rules: [
@@ -13,37 +17,8 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            sourceType: 'module'
-          }
         },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    })
-  ],
 };
